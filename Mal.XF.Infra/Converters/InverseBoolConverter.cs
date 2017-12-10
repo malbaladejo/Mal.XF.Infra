dfdf72188cee2094297dcd4124af37e2b26da205
malbaladejo <malbaladejo@free.fr>
2017-12-10 08:25:20 +0100

    InverseBoolConverter

---
+++ Mal.XF.Infra/Converters/InverseBoolConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Mal.XF.Infra.Converters
+{
+    public class InverseBoolConverter : IValueConverter
+    {
+        private static Lazy<InverseBoolConverter> instance = new Lazy<InverseBoolConverter>(() => new InverseBoolConverter());
+        public static InverseBoolConverter Instance => instance.Value;
+
+        public bool Convert(bool value)
+            => !value;
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+                return this.Convert((bool)value);
+
+            return value;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+                return this.Convert((bool)value);
+
+            return value;
+        }
+    }
+}
---
+++ Mal.XF.Infra/PlatformInitializers/UwpInitializerBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace Mal.XF.Infra
+{
+    public class UwpInitializerBase : IPlatformInitializer
+    {
+        public virtual void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}

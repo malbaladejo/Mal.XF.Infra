03fd1b563e27312db8314faf1c47ccb9f31552a8
Michael Albaladejo <m.albaladejo@lombardodier.com>
2018-01-09 07:59:58 +0100

    FontAwesome

---
+++ Mal.XF.Infra.Android/Renderers/IconLabelRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Android.Graphics;
+using Mal.XF.Infra.Android.Renderers;
+using Mal.XF.Infra.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(IconLabel), typeof(IconLabelRenderer))]
+
+namespace Mal.XF.Infra.Android.Renderers
+{
+
+    public class IconLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                //The ttf in /Assets is CaseSensitive, so name it FontAwesome.ttf
+                Control.Typeface = Typeface.CreateFromAsset(Forms.Context.Assets, IconLabel.TypefaceFileName);
+            }
+        }
+    }
+}
---
+++ Mal.XF.Infra/Controls/IconLabel.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace Mal.XF.Infra.Controls
+{
+    public class IconLabel : Label
+    {
+        public static readonly string Typeface = "Font-Awesome-5-Free-Regular-400";
+        public static readonly string TypefaceFileName = "Font-Awesome-5-Free-Regular-400.otf";
+
+        private string FontFamilyName => $"{TypefaceFileName}#{Typeface}";
+
+        public IconLabel()
+        {
+            FontFamily = FontFamilyName;
+        }
+
+        public IconLabel(string text = null)
+        {
+            FontFamily = FontFamilyName;
+            Text = text;
+        }
+    }
+}
